bc5b288fa163a25a880cce2a70e187d02ddb09ec
Mr.Duan Tools <Mr.Duan Tools>
2017-04-19 23:12:55 +0800

    红外线遥控编码解析

--- SmartCar.ino
+++ SmartCar.ino
@@ -1,12 +1,24 @@
 #include "Smart.h"
+#include "IRremote.h"
 
 SmartCar car;
+
+int RECV_PIN = 13; // 红外一体化接收头连接到Arduino 11号引脚
+ 
+IRrecv irrecv(RECV_PIN);
+ 
+decode_results results; // 用于存储编码结果的对象
 
 void setup(){
   delay(3000);
   car.initSmartCar();
+
+   irrecv.enableIRIn(); //初始化红外遥控
 }
 
 void loop(){
-  car.goForward();
+   if (irrecv.decode(&results)) {
+        Serial.println(results.value);
+        irrecv.resume(); // 接收下一个编码
+   }
 }
