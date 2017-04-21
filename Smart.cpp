8f8e8919cb16a1f17706594cd919d5457f41809b
DuanEnJian <duanenjian@300.cn>
2017-04-21 18:02:00 +0800

    添加超声波探测距离

--- Smart.cpp
+++ Smart.cpp
@@ -1,15 +1,48 @@
 #include "Smart.h"
 
+/**
+ * 直流电机1
+ * @Author   DuanEnJian<backtrack843@163.com>
+ * @DateTime 2017-04-21
+ */
 static AF_DCMotor motor1(1, MOTOR12_1KHZ);
+
+/**
+ * 直流电机2
+ * @Author   DuanEnJian<backtrack843@163.com>
+ * @DateTime 2017-04-21
+ */
 static AF_DCMotor motor2(2, MOTOR12_1KHZ); 
+
+/**
+ * 直流电机3
+ * @Author   DuanEnJian<backtrack843@163.com>
+ * @DateTime 2017-04-21
+ */
 static AF_DCMotor motor3(3, MOTOR34_1KHZ); 
+
+/**
+ * 直流电机4
+ * @Author   DuanEnJian<backtrack843@163.com>
+ * @DateTime 2017-04-21
+ */
 static AF_DCMotor motor4(4, MOTOR34_1KHZ);
 
-
+/**
+ * 构造函数
+ * @Author   DuanEnJian<backtrack843@163.com>
+ * @DateTime 2017-04-21
+ */
 SmartCar::SmartCar(void){
   
 }
 
+/**
+ * 日志输出
+ * @Author   DuanEnJian<backtrack843@163.com>
+ * @DateTime 2017-04-21
+ * @param    log                              日志
+ */
 void SmartCar::logs(const char* log){
     if(IS_DEBUG){
         Serial.println(log);
@@ -18,6 +51,8 @@ void SmartCar::logs(const char* log){
 
 /**
  * 初始化小车 setup调用
+ * @Author   DuanEnJian<backtrack843@163.com>
+ * @DateTime 2017-04-21
  */
 void SmartCar::initSmartCar(void){
     //设置通信串口
@@ -27,6 +62,7 @@ void SmartCar::initSmartCar(void){
     //设置舵机串口
     this->servo1.attach(STEERING_ONE);
     this->servo2.attach(STEERING_TWO);
+    this->servo3.attach(STEERING_THREE);
     this->logs("MSG: servo ok");
     //设置马达转速
     motor1.setSpeed(MOTOR_SPEED);
@@ -34,10 +70,16 @@ void SmartCar::initSmartCar(void){
     motor3.setSpeed(MOTOR_SPEED);
     motor4.setSpeed(MOTOR_SPEED);
     this->logs("MSG: motor ok");
+    //定义超声波接口
+    pinMode(ULTRASONIC_TRIG, OUTPUT);  
+    pinMode(ULTRASONIC_ECHO, INPUT);
+    this->logs("MSG: ultrasonic ok");
 }
 
 /**
- * 前进
+ * 小车前进
+ * @Author   DuanEnJian<backtrack843@163.com>
+ * @DateTime 2017-04-21
  */
 void SmartCar::goForward(void){
     this->logs("MSG: motor ↑");
@@ -48,7 +90,9 @@ void SmartCar::goForward(void){
 }
 
 /**
- * 后退
+ * 小车后退
+ * @Author   DuanEnJian<backtrack843@163.com>
+ * @DateTime 2017-04-21
  */
 void SmartCar::goBack(void){
     this->logs("MSG: motor ↓");
@@ -59,7 +103,9 @@ void SmartCar::goBack(void){
 }
 
 /**
- * 停止
+ * 小车停止
+ * @Author   DuanEnJian<backtrack843@163.com>
+ * @DateTime 2017-04-21
  */
 void SmartCar::stopMotor(void){
     this->logs("MSG: motor stop");
@@ -70,7 +116,9 @@ void SmartCar::stopMotor(void){
 }
 
 /**
- * 左转
+ * 小车左转
+ * @Author   DuanEnJian<backtrack843@163.com>
+ * @DateTime 2017-04-21
  */
 void SmartCar::goTurnLeft(void){
     this->logs("MSG: motor ←");
@@ -81,7 +129,9 @@ void SmartCar::goTurnLeft(void){
 }
 
 /**
- * 右转
+ * 小车右转
+ * @Author   DuanEnJian<backtrack843@163.com>
+ * @DateTime 2017-04-21
  */
 void SmartCar::goTurnRight(void){
     this->logs("MSG: motor →");
@@ -93,39 +143,114 @@ void SmartCar::goTurnRight(void){
 
 /**
  * 相机水平转动
- * @params int number 转动角度 0-180
+ * @Author   DuanEnJian<backtrack843@163.com>
+ * @DateTime 2017-04-21
+ * @param    number                           转动角度 0-180
  */
 void SmartCar::cameraHTurn(int number){
     this->logs("MSG: servo horizontal move ");
-    int current_pos = servo1.read();
+    this->steeringTurn(&this->servo1,&this->pos1,number);
+//    int current_pos = servo1.read();
+//    if(current_pos < number){
+//        for(this->pos1 = current_pos; this->pos1 < number; this->pos1 += 1) {
+//          servo1.write(this->pos1);
+//          delay(15);
+//        }
+//    }else{
+//        for(this->pos1 = current_pos; this->pos1>=number; this->pos1-=1){                                
+//          servo1.write(this->pos1);
+//          delay(15);
+//        } 
+//    }
+}
+
+/**
+ * 相机上下转动
+ * @Author   DuanEnJian<backtrack843@163.com>
+ * @DateTime 2017-04-21
+ * @param    number                           转动角度 0-180
+ */
+void SmartCar::cameraVTurn(int number){
+    this->logs("MSG: servo Vertical move ");
+    this->steeringTurn(&this->servo2,&this->pos2,number);
+//    int current_pos = servo2.read();
+//    if(current_pos < number){
+//        for(this->pos2 = current_pos; this->pos2 < number; this->pos2 += 1) {
+//            servo2.write(this->pos2);
+//            delay(15);
+//        }
+//    }else{
+//        for(this->pos2 = current_pos; this->pos2>=number; this->pos2-=1){                                
+//            servo2.write(this->pos2);
+//            delay(15);
+//        } 
+//    }
+}
+
+/**
+ * 超声波舵机水平转动
+ * @Author   DuanEnJian<backtrack843@163.com>
+ * @DateTime 2017-04-21
+ * @param    number                           转动角度 0-180
+ */
+void SmartCar::ultrasonicVTrun(int number){
+    this->logs("MSG: ultrasonic Vertical move ");
+    this->steeringTurn(&this->servo3,&this->pos3,number);
+}
+
+/**
+ * 舵机转动控制
+ * @Author   DuanEnJian<backtrack843@163.com>
+ * @DateTime 2017-04-21
+ * @param    servo                            Servo对象
+ * @param    pos                              舵机当前角度 0-180
+ * @param    number                           转动角度 0-180
+ */
+void SmartCar::steeringTurn(Servo* servo, int* pos, int number){
+    int current_pos = servo->read();
     if(current_pos < number){
-        for(this->pos1 = current_pos; this->pos1 < number; this->pos1 += 1) {
-          servo1.write(this->pos1);
-          delay(15);
+        for(*pos = current_pos; *pos < number; *pos += 1) {
+            servo->write(*pos);
+            delay(15);
         }
     }else{
-        for(this->pos1 = current_pos; this->pos1>=number; this->pos1-=1){                                
-          servo1.write(this->pos1);
-          delay(15);
+        for(*pos = current_pos; *pos>=number; *pos-=1){                                
+            servo->write(*pos);
+            delay(15);
         } 
     }
 }
 
 /**
- * 相机上下转动
+ * 超声波测距
+ * @Author   DuanEnJian<backtrack843@163.com>
+ * @DateTime 2017-04-21
+ * @return   float 返回距离(单位cm)
  */
-void SmartCar::cameraVTurn(int number){
-    this->logs("MSG: servo Vertical move ");
-    int current_pos = servo2.read();
-    if(current_pos < number){
-        for(this->pos2 = current_pos; this->pos2 < number; this->pos2 += 1) {
-            servo2.write(this->pos2);
-            delay(15);
-        }
-    }else{
-        for(this->pos2 = current_pos; this->pos2>=number; this->pos2-=1){                                
-            servo2.write(this->pos2);
-            delay(15);
-        } 
-    }
+float SmartCar::detectionRange(){
+    float temp,cm;
+    //给Trig发送一个低高低的短时间脉冲,触发测距 
+    //给Trig发送一个低电平   
+    digitalWrite(ULTRASONIC_TRIG, LOW);
+    //等待 2微妙 
+    delayMicroseconds(2);
+    //给Trig发送一个高电平      
+    digitalWrite(ULTRASONIC_TRIG,HIGH);
+    //等待 10微妙   
+    delayMicroseconds(10);
+    //给Trig发送一个低电平      
+    digitalWrite(ULTRASONIC_TRIG, LOW);   
+    //存储回波等待时间 pulseIn函数会等待引脚变为HIGH,开始计算时间,再等待变为LOW并停止计时并返回脉冲的长度
+    temp = float(pulseIn(ULTRASONIC_ECHO, HIGH));
+
+    /**
+     * 声速是:340m/1s 换算成 34000cm / 1000000μs => 34 / 1000
+     * 因为发送到接收,实际是相同距离走了2回,所以要除以2
+     * 距离(厘米)  =  (回波时间 * (34 / 1000)) / 2
+     * 简化后的计算公式为 (回波时间 * 17)/ 1000
+     */
+    
+    //把回波时间换算成cm
+    cm = (temp * 17 )/1000;   
+    return cm;
 }
--- Smart.h
+++ Smart.h
@@ -13,8 +13,14 @@
 #define STEERING_ONE 9
 //定义舵机2串口
 #define STEERING_TWO 10
+//定义舵机3串口(超声波)
+#define STEERING_THREE A0
 //定义马达速度
 #define MOTOR_SPEED 200
+//定义超声波Tring引脚
+#define ULTRASONIC_TRIG A1   
+//定义超声波Echo引脚
+#define ULTRASONIC_ECHO A2
 
 class SmartCar{
     public:
@@ -22,15 +28,18 @@ class SmartCar{
         void initSmartCar(void);
         void cameraHTurn(int number);
         void cameraVTurn(int number);
+        void ultrasonicVTrun(int number);
         void goTurnLeft(void);
+        float detectionRange();
         void goTurnRight(void);
         void goForward(void);
         void goBack(void);
         void stopMotor(void);
         void logs(const char* log);
     private:
-        Servo servo1,servo2;  
-        int pos1=0,pos2=0;
+        Servo servo1,servo2,servo3;  
+        int pos1=0,pos2=0,pos3=0;
+        void steeringTurn(Servo* servo, int* pos, int number);
 };
 
 #endif
