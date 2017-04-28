1eca765b7e90c95245d73dfb0bfd877353f89b91
DuanEnJian <duanenjian@300.cn>
2017-04-28 13:38:00 +0800

    修改项目结构

---
+++ Service/SQL/smart_car.sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: 2017-04-28 13:32:49
+-- 服务器版本： 5.6.29-log
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `smart_car`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `device`
+--
+
+CREATE TABLE `device` (
+  `id` int(11) NOT NULL COMMENT '设备ID',
+  `name` varchar(32) NOT NULL COMMENT '设备名称',
+  `mac` varchar(17) NOT NULL COMMENT '设备MAC地址',
+  `status` tinyint(4) NOT NULL COMMENT '设备状态'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='设备表';
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL COMMENT '用户ID',
+  `username` varchar(32) NOT NULL COMMENT '用户账号',
+  `password` varchar(32) NOT NULL COMMENT '用户密码',
+  `nickname` varchar(32) NOT NULL COMMENT '用户昵称',
+  `head` varchar(128) DEFAULT NULL COMMENT '用户头像'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='用户表';
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `device`
+--
+ALTER TABLE `device`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`),
+  ADD UNIQUE KEY `mac` (`mac`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `nickname` (`nickname`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `device`
+--
+ALTER TABLE `device`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '设备ID';
+--
+-- 使用表AUTO_INCREMENT `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户ID';COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
