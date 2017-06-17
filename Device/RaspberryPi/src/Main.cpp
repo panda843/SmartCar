0bd15ca6800ac3c99a34a0bc57b4f0e5b6a51d4e
Tools <tools@bogon.dhcp>
2017-06-17 13:26:12 +0800

    测试串口通信

--- Device/RaspberryPi/src/Main.cpp
+++ Device/RaspberryPi/src/Main.cpp
@@ -3,11 +3,11 @@
 //设置码率
 void set_speed(int fd, int speed){
     int speed_arr[] = {B38400, B19200, B9600, B4800, B2400, B1200, B300, B38400, B19200, B9600, B4800, B2400, B1200, B300};  
-    int name_arr[] = {38400,  19200,  9600,  4800,  2400,  1200,  300, 38400,  19200,  9600, 4800, 2400, 1200,  300, };   
+    int name_arr[] = {38400,  19200,  9600,  4800,  2400,  1200,  300, 38400,  19200,  9600, 4800, 2400, 1200,  300};   
     struct termios   Opt; 
     int i; 
     tcgetattr(fd, &Opt);  
-    for(i= 0;  i < sizeof(speed_arr)/sizeof(int);  i++){  
+    for(i= 0;  i < 14;  i++){  
         if(speed == name_arr[i]){  
             tcflush(fd, TCIOFLUSH);  
             cfsetispeed(&Opt, speed_arr[i]);  
@@ -127,6 +127,7 @@ void readArduinoData(char* read_buf){
             if(readnum > 0 && readnum < 8){  
                 read_nBytes[readnum] = '\0';
                 strcat(read_buf,read_nBytes);
+                printf("read:%s\n",read_buf);
                 break;  
             } 
         }
